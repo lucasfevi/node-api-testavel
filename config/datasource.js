494383c0410dbf5139503747c188e82a268a6e74
Lucas Vieira <newlucasfevi@gmail.com>
2017-02-03 16:51:04 -0200

    Add config files

---
+++ config/datasource.js
@@ -0,0 +1,29 @@
+import fs from 'fs';
+import path from  'path';
+import Sequelize from 'sequelize';
+
+let database = null;
+
+export default (app) => {
+  if (!database) {
+    const config = app.config,
+      sequelize = new Sequelize(
+        config.database,
+        config.username,
+        config.password,
+        config.params
+      );
+
+    database = {
+      sequelize,
+      Sequelize,
+      models: {}
+    };
+
+    sequelize.sync().done() => {
+      return database;
+    };
+  }
+
+  return database;
+};
